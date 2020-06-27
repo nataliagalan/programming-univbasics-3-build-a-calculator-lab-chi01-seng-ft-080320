97602025ef435ea0f0a298f6497890cb2668fff7
Natalia Galan <n8galan@gmail.com>
2020-06-27 23:08:39 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,33 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
 
 def division(num1, num2)
-
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-
+ num1 * num2
 end
 
 def modulo(num1, num2)
-
+ num1 % num2
 end
 
 def square_root(num)
 
+  if num == 0 || num == 1
+    return num
+  end
+
+  counter = 1
+  while (counter * counter) <= num
+  counter += 1
+  end
+  
+counter - 1
 end
